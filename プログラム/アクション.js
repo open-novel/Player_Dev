7a72b50d00fbe75821c491a2ad0744714a4e63a9
open-novel <32639927+open-novel@users.noreply.github.com>
2019-05-27 01:29:30 +0900

    u

--- プログラム/アクション.js
+++ プログラム/アクション.js
@@ -593,7 +593,7 @@ function decoText ( text ) {
 				break;	case 'C': color = undefined
 				break;	case 's': mag = val
 				break;	case 'S': mag = 1
-				break;	case '_': mag = 1
+				break;	case '_':
 				break;	default : $.warn( `"${ type }" このメタ文字は未実装です`　)
 			}
 		} else {
